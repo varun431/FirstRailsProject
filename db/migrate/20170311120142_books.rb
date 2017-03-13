4dcff3b73b57c4e105b02911ceed111a2c98e156
Varun Bumb <varun.270695@gmail.com>
2017-03-13 19:22:08 +0530

    Initial commit

---
+++ db/migrate/20170311120142_books.rb
@@ -0,0 +1,18 @@
+class Books < ActiveRecord::Migration[5.1]
+  def self.up
+    if table_exists?(:books)
+      drop_table :books
+    end
+    create_table :books do |t|
+      t.column :title, :string, :limit => 32, :null => false
+      t.column :price, :float
+      t.column :subject_id, :integer
+      t.column :description, :text
+      t.column :created_at, :timestamp
+    end
+  end
+
+  def self.down
+    drop_table :books
+  end
+end
